5a66b904614f71a39e99ad4c755fffba00a6ae60
arthurrodrigues1 <167815228+arthurrodrigues1@users.noreply.github.com>
2024-11-29 20:16:02 -0300

    comecei a pagina de adicionar

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-let 
+let itens = [
+    
+]
+
+function novoitem(){
+    let input = document.getElementById("iteminput")
+    let novoitem = input.value
+    
+    if (novoitem === ""){
+        alert("por favor, insira um item")
+    }
+
+    let lista = document.getElementById
+
+    input.value = ""
+}
