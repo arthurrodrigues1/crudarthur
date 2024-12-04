cdff0ed669d209d4ebae970e2b7eed6141bd0552
arthurrodrigues1 <167815228+arthurrodrigues1@users.noreply.github.com>
2024-12-04 20:47:29 -0300

    comecei do zero com apenas um html

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-function adicionaritem(){
-    let input = document.getElementById("iteminput")
-    let novoitem = input.value
-    
-    if (novoitem === ""){
-        alert("por favor, insira um item")
-    }
-    criartd()
-}
 
-function criartd(){
-    let tabela = getElementById("tabela")
-    tabela.appendChild(document.createElement(`<tr><td>${novoitem}</td></tr>`))
-}
+
+let nomeItem = document.getElementById('nomeItem')
