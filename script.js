7cf8343a09d33256c28b41dfe49f50f6ee214682
arthurrodrigues1 <167815228+arthurrodrigues1@users.noreply.github.com>
2024-12-04 21:59:38 -0300

    a

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-
-
-let nomeItem = document.getElementById('nomeItem')
+function adicionar() {
+    let nomeItem = document.getElementById('nomeItem');
+    let novoItem = nomeItem.value
+    
+    let lista = document.getElementById('lista');
+    lista.appendChild(document.createElement(`<li>${novoItem}</li>`))
+}
